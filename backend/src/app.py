006d52ae417b63b17c391b998401cda1fb8ae356
mayarakaren <mayarakaren5910@gmail.com>
2024-02-29 16:03:21 -0300

    V3

---
+++ backend/src/app.py
@@ -0,0 +1,62 @@
+""" from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+import os
+
+app = Flask(__name__)
+template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../frontend/templates'))
+app.template_folder = template_dir
+
+DATABASE = 'preencheAqui.db'
+
+def create_table():
+    with sqlite3.connect(DATABASE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS pessoas (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nomeCompleto TEXT,
+                dataNascimento DATE,
+                idade INTEGER,
+                cpf TEXT,
+                email TEXT,
+                telefone TEXT,
+                estadoCivil TEXT,
+                endereco TEXT,
+                cep TEXT
+            )
+        ''')
+
+create_table()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/salvar', methods=['POST'])
+def salvar():
+    if request.method == 'POST':
+        dados_formulario = request.form
+        salvar_no_banco(dados_formulario)
+    return redirect(url_for('index'))
+
+def salvar_no_banco(dados_formulario):
+    with sqlite3.connect(DATABASE) as connection:
+        cursor = connection.cursor()
+        cursor.execute('''
+            INSERT INTO pessoas (nomeCompleto, dataNascimento, idade, cpf, email, telefone, estadoCivil, endereco, cep)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            dados_formulario['nomeCompleto'],
+            dados_formulario['dataNascimento'],
+            dados_formulario['idade'],
+            dados_formulario['cpf'],
+            dados_formulario['email'],
+            dados_formulario['telefone'],
+            dados_formulario['estadoCivil'],
+            dados_formulario['endereco'],
+            dados_formulario['cep']
+        ))
+        connection.commit()
+
+if __name__ == '__main__':
+    app.run(debug=True) """
---
+++ src/js/app.js
@@ -0,0 +1,117 @@
+const inputTelefone = document.getElementById("telefone");
+
+function formatarTelefone(numero) {
+    numero = numero.replace(/[^0-9]/g, "");
+
+    if (numero.length === 11) {
+        return `(${numero.slice(0, 2)}) ${numero.slice(2, 7)}-${numero.slice(7)}`;
+    }
+
+    return `(${numero.slice(0, 2)}) ${numero.slice(2)}`;
+}
+
+inputTelefone.addEventListener("input", (event) => {
+    const valorAtual = event.target.value;
+    const valorFormatado = formatarTelefone(valorAtual);
+    inputTelefone.value = valorFormatado;
+});
+
+//CPF
+function formatarCPF(numero) {
+    numero = numero.replace(/\D/g, "");
+
+    numero = numero.slice(0, 11);
+    if (numero.length === 11) {
+        return `${numero.slice(0, 3)}.${numero.slice(3, 6)}.${numero.slice(
+            6,
+            9
+        )}-${numero.slice(9, 11)}`;
+    } else {
+        return `${numero.slice(0, 3)}.${numero.slice(3, 6)}.${numero.slice(6, 9)}-${numero.slice(9, 11)}`;
+    }
+}
+const inputCPF = document.getElementById("cpf");
+
+inputCPF.addEventListener("input", (event) => {
+    const valorAtual = event.target.value;
+    const valorFormatado = formatarCPF(valorAtual);
+
+    inputCPF.value = valorFormatado;
+});
+
+//CEP
+function formatarCEP(cep) {
+    cep = cep.replace(/\D/g, '');
+    if (cep.length === 8) {
+        return `${cep.slice(0, 5)}-${cep.slice(5)}`;
+    }
+    return cep;
+}
+const inputCEP = document.getElementById('cep');
+
+inputCEP.addEventListener('input', (event) => {
+    const valorAtual = event.target.value;
+    const valorFormatado = formatarCEP(valorAtual);
+
+    inputCEP.value = valorFormatado;
+});
+
+//Limpar formulário
+function limparFormulario() {
+    document.getElementById('signupForm').reset();
+}
+
+//Validações
+
+function validarCPF(cpf) {
+    const regex = /^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\-]?[0-9]{2})$/;
+    return regex.test(cpf);
+  }
+
+  function validarCEP(cep) {
+    const regex = /^([0-9]{5}[\-]?[0-9]{3})$/;
+    return regex.test(cep);
+  }
+
+  function validarEmail(email) {
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
+  }
+  
+  function validar() {
+    const cpf = document.getElementById("cpf").value;
+    const cep = document.getElementById("cep").value;
+    const email = document.getElementById("email").value;
+  
+    if (!validarCPF(cpf)) {
+      alert("CPF inválido!");
+      return;
+    }
+  
+    if (!validarCEP(cep)) {
+      alert("CEP inválido!");
+      return;
+    }
+  
+    if (!validarEmail(email)) {
+      alert("Email inválido!");
+      return;
+    }
+  
+    alert("Dados válidos!");
+  }
+
+  //idade
+  document.getElementById('dataNascimento').addEventListener('change', function() {
+
+    var dataNascimento = new Date(this.value);
+    var dataAtual = new Date();
+    var diffAnos = dataAtual.getFullYear() - dataNascimento.getFullYear();
+
+    if (dataAtual.getMonth() < dataNascimento.getMonth() || 
+        (dataAtual.getMonth() === dataNascimento.getMonth() && dataAtual.getDate() < dataNascimento.getDate())) {
+        diffAnos--;
+    }
+
+    document.getElementById('idade').value = diffAnos;
+});
