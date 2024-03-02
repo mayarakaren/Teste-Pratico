0c7147ad7f4617c57f59c7eb82143fcb2e1179a7
mayarakaren <mayarakaren5910@gmail.com>
2024-03-02 19:41:09 -0300

    V8

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,15 +1,11 @@
-# controllers/auth.py
 from flask_login import LoginManager, UserMixin
-from models.database import db, Pessoa
+from models.database import Pessoa  
 
 login_manager = LoginManager()
 login_manager.login_view = 'routes.login'
 
-class User(db.Model, UserMixin):
-    __tablename__ = 'pessoa'  
-    id = db.Column(db.Integer, primary_key=True)
-    nome = db.Column(db.String(255))
-    senha = db.Column(db.String(255))
+class User(UserMixin):
+    pass
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -19,3 +15,25 @@ def create_user_from_pessoa(pessoa):
     user = User()
     user.id = pessoa.id
     return user
+
+# # controllers/auth.py
+# from flask_login import LoginManager, UserMixin
+# from models.database import db, Pessoa
+
+# login_manager = LoginManager()
+# login_manager.login_view = 'routes.login'
+
+# class User(db.Model, UserMixin):
+#     __tablename__ = 'pessoa'  
+#     id = db.Column(db.Integer, primary_key=True)
+#     nome = db.Column(db.String(255))
+#     senha = db.Column(db.String(255))
+
+# @login_manager.user_loader
+# def load_user(user_id):
+#     return User.query.get(int(user_id))
+
+# def create_user_from_pessoa(pessoa):
+#     user = User()
+#     user.id = pessoa.id
+#     return user
